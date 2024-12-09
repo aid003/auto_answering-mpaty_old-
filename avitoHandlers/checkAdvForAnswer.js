5fc5eb499305d9fea38a0e10a1283602016de7e6
aid003 <teremochek859@gmail.com>
2024-12-09 22:08:12 +0300

    wefrgt334

---
+++ avitoHandlers/checkAdvForAnswer.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function chekIdAdvForAnswer(idAdv) {
+  let message = "";
+  const currentIdAdv = [
+    { id: 4516692170, type: "telegram" },
+    { id: 4517441116, type: "telegram" },
+  ];
+
+  //   { id: 1, typeAdvertizing: 'all', text: 'внешка' },
+  //   { id: 2, typeAdvertizing: 'avitolog', text: 'Авитолог' },
+  //   { id: 3, typeAdvertizing: 'telegram', text: 'телега' },
+  //   { id: 4, typeAdvertizing: 'analit', text: 'аналитика' }
+
+  for (let adv of currentIdAdv) {
+    if (adv.id === idAdv) {
+      message = await prisma.messages.findUnique({
+        where: {
+          typeAdvertizing: adv.type,
+        },
+        select: {
+          text: true,
+        },
+      });
+    } else {
+      message = await prisma.messages.findUnique({
+        where: {
+          typeAdvertizing: "all",
+        },
+        select: {
+          text: true,
+        },
+      });
+    }
+  }
+
+  return message.text;
+}
